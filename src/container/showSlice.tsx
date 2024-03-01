fe69867a9f8a2a8f125664a8809fd86718c3798d
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-03-01 12:03:44 +0600

    added fetch request & extra reducers to the show slice

--- src/container/showSlice.tsx
+++ src/container/showSlice.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const ShowSlice: React.FC = () => {
-    return (
-        <div>
+export const fetchShows = createAsyncThunk('tvShow/fetchShows', async (query) => {
+    const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
+    return response.data;
+});
 
-        </div>
-    );
-};
+export const fetchShowDetail = createAsyncThunk('tvShow/fetchShowDetails', async (id) => {
+    const response = await axios.get(`http://api.tvmaze.com/shows/${id}`);
+    return response.data;
+});
 
-export default ShowSlice;
+export const tvShowSlice = createSlice({
+    name: 'tvShow',
+    initialState: { shows: [], showDetail: null, status: 'idle', error: null },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchShows.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchShows.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.shows = action.payload;
+            })
+            .addCase(fetchShows.rejected, (state) => {
+                state.status = 'failed';
+            })
+            .addCase(fetchShowDetail.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchShowDetail.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.showDetail = action.payload;
+            })
+            .addCase(fetchShowDetail.rejected, (state) => {
+                state.status = 'failed';
+            });
+    },
+});
+
+export default tvShowSlice.reducer;
