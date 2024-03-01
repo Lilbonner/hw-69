fc16e0e640adfc97449c8a3883327556e92b8f60
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-03-01 12:21:46 +0600

    add routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Search from "./components/Search.tsx";
+import Detail from "./components/Detail.tsx";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <Search />
-      <div>
-
-      </div>
+          <Routes>
+                  <Route path="/shows/:id" element={<Detail />} />
+                  <Route path="/" element={<Search />} />
+          </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {Provider} from "react-redux";
+import store from "./App/Store.ts";
 import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <BrowserRouter>
+      <Provider store={store}>
+          <BrowserRouter>
           <App />
-      </BrowserRouter>
+          </BrowserRouter>
+      </Provider>
   </React.StrictMode>,
 )
