226d8623f299eeb1ff6259d89462c95bce268a6e
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-03-01 12:42:04 +0600

    Added data typing and processing in ShowDetail component

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchShowDetail } from '../container/showSlice.tsx';
+import Search from "./Search.tsx";
 
-const Detail:React.FC = () => {
+
+function ShowDetail() {
+    const { id } = useParams<{ id: string }>();
+    const dispatch = useDispatch();
+    const showDetail = useSelector((state: any) => state.tvShow.showDetail);
+
+    useEffect(() => {
+        dispatch(fetchShowDetail(id));
+    }, [dispatch, id]);
+
+    if (!showDetail) {
+        return <div>Loading...</div>;
+    }
+
+
     return (
         <div>
+            <Search/>
+            <div className='flex'>
+                <img alt='img' className='ml-5 mt-10 rounded-md' src={showDetail.image.medium}/>
+                <div className="mt-14 ml-6">
+                    <h2 className='text-3xl '>{showDetail.name}</h2>
+                    <p className='mt-4 text-2xl'>{showDetail.summary}</p>
+                </div>
+            </div>
 
         </div>
     );
-};
+}
 
-export default Detail;
+export default ShowDetail;
